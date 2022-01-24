d146dc3a8d52a2f7675f66b624f9f0cf453fb3d1
Andrej Pedchenko <graduadu@gmail.com>
2022-01-24 13:20:30 +0200

    Added save token value to js file.

--- app/weatherCLI/src/app.js
+++ app/weatherCLI/src/app.js
@@ -3,6 +3,15 @@ import {getArgs} from './helpers/args.js'
 import {printHelp, printSuccess, printError} from './services/log.service.js'
 import {saveKeyValue} from './services/storage.service.js'
 
+const saveToken = async (token) => {
+  try {
+    await saveKeyValue('token', token)
+    printSuccess('Токен сохранен')
+  } catch (e) {
+    printError(e.message)
+  }
+}
+
 const initCLI = () => {
   const args = getArgs(process.argv)
   if (args.h) {
@@ -10,16 +19,15 @@ const initCLI = () => {
     // Вывод help
   }
   if (args.s) {
-    printSuccess(args.s)
+    // printSuccess(args.s)
     // Сохранить название города
   }
   if (args.t) {
-    saveKeyValue('token', args.t)
-    printSuccess(args.t)
-    // Сохранить токен
+    saveToken(args.t)
+    // printSuccess(args.t)
   }
 
-  printError('Some error')
+  // printError('Some error')
   // вывести погоду
 }
 
--- app/weatherCLI/src/services/storage.service.js
+++ app/weatherCLI/src/services/storage.service.js
@@ -1,15 +1,45 @@
 import {homedir} from 'os'
 import {join, dirname, extname, relative, isAbsolute, resolve, sep} from 'path'
+import {promises} from 'fs'
 
-const filePath = join(homedir(), 'weather-data.json')
+// const filePath = join(homedir(), 'weather-data.json') // on linux dir '/root'
+const filePath = process.env.STORAGE_PATH + '/.weather-data.json'
 
-const saveKeyValue = (key, value) => {
-  console.log(`filePath: ${filePath}`)
-  console.log(key, value)
+const saveKeyValue = async (key, value) => {
+  let data = {}
+  if (await isExists(filePath)) {
+    const file = await promises.readFile(filePath)
+    data = JSON.parse(file)
+  }
+
+  data[key] = value
+
+  await promises.writeFile(filePath, JSON.stringify(data))
+}
+
+const getKeyValue = async (key) => {
+  if (await isExists(filePath)) {
+    const file = await promises.readFile(filePath)
+    const data = JSON.parse(file)
+    return data[key]
+  }
+  return undefined
+}
+
+const isExists = async (path) => {
+  try {
+    await promises.stat(path)
+    return true
+
+  // eslint-disable-next-line no-unused-vars
+  } catch (e) {
+    return false
+  }
 }
 
 // eslint-disable-next-line no-unused-vars
 const examples = () => {
+  console.log(join(homedir(), 'weather-data.json')) // on linux dir '/root'
   console.log(`filePath: ${filePath}`)
   console.log(`dirname: ${dirname(filePath)}`)
   console.log(`extname: ${extname(filePath)}`)
@@ -19,4 +49,4 @@ const examples = () => {
   console.log(`sep: ${sep}`)
 }
 
-export {saveKeyValue}
+export {saveKeyValue, getKeyValue}

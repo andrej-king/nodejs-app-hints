05c138a6d7d341bf8917fbda3ff9e2a7f6573118
Andrej Pedchenko <graduadu@gmail.com>
2022-01-28 12:39:32 +0200

    Added rote types

--- app/dashboard-api/src/users/users.ts
+++ app/dashboard-api/src/users/users.ts
@@ -1,18 +1,18 @@
-import express from 'express'
+import express, {Request, Response, NextFunction} from 'express'
 
 const userRouter = express.Router()
 
 // обработчик роутов users
-userRouter.use((req, res, next) => {
+userRouter.use((req: Request, res: Response, next: NextFunction) => {
   console.log('Обработчик users', Date.now())
   next()
 })
 
-userRouter.post('/login', (req, res) => {
+userRouter.post('/login', (req: Request, res: Response) => {
   res.send('login')
 })
 
-userRouter.post('/register', (req, res) => {
+userRouter.post('/register', (req: Request, res: Response) => {
   res.send('register')
 })
 

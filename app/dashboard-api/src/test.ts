c2fccc743527f65ff0b914fc0409713f3480288a
Andrej Pedchenko <graduadu@gmail.com>
2022-01-28 12:34:49 +0200

    Added typescript example interfaces and types.

--- app/dashboard-api/src/test.ts
+++ app/dashboard-api/src/test.ts
@@ -1,4 +1,4 @@
-//region basic types
+//region Basic types
 let a: number = 5
 let b: string = 'abc'
 let c: string = '4'
@@ -32,7 +32,7 @@ function coordinates(coord: { lat: number, long?: number }) {
 
 //endregion
 
-//region union types
+//region Union types
 let universalId: number | string = 5
 universalId = 'sdsds'
 
@@ -55,4 +55,70 @@ function helloUser(user: string | string[]) {
     console.log(user + 'Hi')
   }
 }
+
+//endregion
+
+//region Interfaces and Types
+type coord = { lat: number, long: number }
+
+interface ICoord {
+  lat: number
+  long: number
+}
+
+type UniqueId = number | string
+type myString = string
+
+function computWithTypeParam(coord: coord) {
+}
+
+function computWithInterfaceParam(coord: ICoord) {
+}
+
+//region Extend interface
+interface IAnimal {
+  name: string
+}
+
+interface IDog extends IAnimal {
+  tail?: boolean
+}
+
+const dogFromInterface: IDog = {
+  name: 'dsa'
+}
+//endregion
+
+//region Extend type
+type AnimalType = {
+  name: string
+}
+
+type DogType = AnimalType & {
+  tail: boolean
+}
+
+const dogFromType: DogType = {
+  name: 'some name',
+  tail: true
+}
+//endregion
+
+//region Merge interface fields
+interface IDogMerge {
+  name: string
+}
+
+interface IDogMerge {
+  tail: boolean
+}
+
+// IDogMerge have both fields
+const dogMerge: IDogMerge = {
+  name: 'sdsds',
+  tail: true
+}
+//endregion
+
+
 //endregion

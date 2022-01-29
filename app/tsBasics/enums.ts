4d08af7e606c6d05a216c27a554ce7d3bcaa3b9c
Andrej Pedchenko <graduadu@gmail.com>
2022-01-29 12:59:46 +0200

    Added more examples with enums

--- app/tsBasics/enums.ts
+++ app/tsBasics/enums.ts
@@ -1,8 +1,41 @@
 type direction = 'left' | 'right'
 
+//region enum, простая запись
 enum Direction {
   Left,
   Right
+  // Left= 'left',
+  // Right = 1
 }
 
 console.log(Direction.Left)
+
+function move(direction: Direction) {
+  switch (direction) {
+    case Direction.Left:
+      return -1
+    case Direction.Right:
+      return 1
+    default:
+      return 0
+  }
+}
+
+//endregion
+
+//region enum могут быть переданы как объекты
+function objMod(obj: { Left: number }) {
+
+}
+
+objMod(Direction)
+//endregion
+
+//region enum в виде константы
+const enum Direction2 {
+  Up,
+  Down
+}
+
+let myDirection = Direction2.Up
+//endregion

73921c927ed2cd7d0d84d244b09b712f48f38ace
Andrej Pedchenko <graduadu@gmail.com>
2022-02-01 09:53:23 +0200

    Added abstract controller for routes. Added route interface

---
+++ app/dashboard-api/src/common/base.controller.ts
@@ -0,0 +1,36 @@
+import {LoggerService} from '../logger/logger.service'
+import {Response, Router} from 'express'
+import {IControllerRoute} from './route.interface'
+
+export abstract class BaseController {
+  private readonly _router: Router
+
+  protected constructor(private logger: LoggerService) {
+    this._router = Router()
+  }
+
+  public get router(): Router {
+    return this._router
+  }
+
+  public send<T>(res: Response, code: number, message: T) {
+    res.type('application/json')
+    return res.status(200).json(message)
+  }
+
+  public ok<T>(res: Response, message: T) {
+    return this.send<T>(res, 200, message)
+  }
+
+  public created(res: Response) {
+    return res.status(201)
+  }
+
+  protected bindRoutes(routes: IControllerRoute[]) {
+    for (const route of routes) {
+      this.logger.log(`[${route.method}] ${route.path}`)
+      const handler = route.func.bind(this)
+      this.router[route.method](route.path, route.func, handler)
+    }
+  }
+}

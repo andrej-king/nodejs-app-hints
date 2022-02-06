d05af17aff9de9606f7c83b80f50f5d0e3881b02
Andrej Pedchenko <graduadu@gmail.com>
2022-02-06 20:59:24 +0200

    Added coverage info for e2e tests.

--- app/dashboard-api/jest.e2e.config.ts
+++ app/dashboard-api/jest.e2e.config.ts
@@ -3,8 +3,9 @@ import type {Config} from '@jest/types'
 const config: Config.InitialOptions = {
   verbose: true,
   preset: 'ts-jest',
-  rootDir: './tests',
-  testRegex: '.e2e-spec.ts$'
+  // rootDir: './tests', // dir with tests
+  testRegex: '.e2e-spec.ts$',
+  coverageDirectory: './var/coverage' // where save coverage result
 }
 
 export default config
